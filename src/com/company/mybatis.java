ba374e437c2b229762fe3ad04f7a858b097e75aa
caoding <2271313916@qq.com>
2018-06-24 16:10:29 +0800

    idea 提交

--- src/com/company/mybatis.java
+++ src/com/company/mybatis.java
@@ -4,4 +4,7 @@ package com.company;
  * Created by txwhd on 2018/6/24.
  */
 public class mybatis {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
