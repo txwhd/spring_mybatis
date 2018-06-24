7a8c1d588b201676b764dd24ba31dce624935f51
caoding <2271313916@qq.com>
2018-06-24 18:31:52 +0800

    idea 提交 根据 name 查询多条数据

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -60,6 +61,39 @@ public class Main {
         List<User> users = sqlSession.selectList("test.findUserByName","ding");
 
         System.out.println(users.toString());
+
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void insertUserTest() throws IOException {
+
+        //mybatis 配置文件
+        String resource = "SqlMapperConfig.xml";
+
+        //配置文件流
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+
+        //创建会话工厂
+        SqlSessionFactory sqlSessionFactory =  new SqlSessionFactoryBuilder().build(inputStream);
+
+        //通过工厂得到sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //生成插入数据
+        User user = new User();
+        user.setAddress("浙江杭州");
+        user.setName("王小二");
+        user.setBirsday(new Date());
+
+        //通过sqlSession操作数据库
+        sqlSession.insert("test.insertUser",user);
+
+        System.out.println("插入user的ID值"+user.getId());
+
+        //提交事务
+        sqlSession.commit();
 
         sqlSession.close();
 
