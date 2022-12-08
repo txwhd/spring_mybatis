69eb501ea6bc4b5bf2dfe873233b611ef1711fb3
dingcao <2271313916@qq.com>
2022-12-08 22:27:29 +0800

    测试用例
    测试用例

---
+++ src/com/company/test/IDatasourceServiceTest.java
@@ -0,0 +1,7 @@
+package com.company.po;
+public class T {
+    public static void main(String[] args) {
+        String s = YmsFailureAnalyzerUtil.acquireClassPersionByName("com.google.common.base.Absent");
+        System.out.println(s);
+    }
+}
