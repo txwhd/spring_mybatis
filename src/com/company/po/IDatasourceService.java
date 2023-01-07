efa8f92982c9caa967091f9261c4f756ea5fba85
caoding <2271313916@qq.com>
2023-01-07 12:24:52 +0800

    添加修改记录

--- src/com/company/po/IDatasourceService.java
+++
@@ -1,8 +0,0 @@
-package com.company.po;
-@RemoteCall(value = "iuap-boot-starter-example-provider")
-public interface IDatasourceService {
-
-    public void insertData();
-
-    public void insertRollbackData();
-}
--- src/com/company/test/IDatasourceServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.company.po;
-public class T {
-    public static void main(String[] args) {
-        String s = YmsFailureAnalyzerUtil.acquireClassPersionByName("com.google.common.base.Absent");
-        System.out.println(s);
-    }
-}
